262dc780ff1f5bf216bb19e151c4d4d406150456
eszesgy <eszesgy@gmail.com>
2025-02-01 15:42:49 +0100

    Create file1.php

---
+++ file1.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Media elágazások frontend szinten</title>
+	
+	<link rel="stylesheet" type="text/css" href="stilus.css">
+	
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body>
+	
+	
+	<div style="width:250px;height:250px;margin:100px auto">
+	<label style="position:absolute">Ide húzza az állományt</label>
+	
+  
+	<label class="felirat" for="f1"><i class="fa fa-file"></i></label>
+	<input type="file" id="f1" class="mezo" accept="image/*">
+	
+	</div>
+	
+  </body>
+</html>
+  
